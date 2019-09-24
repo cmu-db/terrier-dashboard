67f34145933262fcf525256db85b4afe7de52fde
Andy Pavlo <pavlo@cs.brown.edu>
2019-09-24 11:46:21 -0400

    Switched to TPCCBenchmark.ScaleFactor4WithLogging

--- jobs/tpcc_line_chart.rb
+++ jobs/tpcc_line_chart.rb
@@ -12,7 +12,7 @@ SCHEDULER.every '1h', :first_in => 0 do |job|
   labels = []
   for build in builds do
     build_url = build['url']
-    build_response = get_url(build_url + 'artifact/script/micro_bench/large_transaction_benchmark.json')
+    build_response = get_url(build_url + 'artifact/script/micro_bench/tpcc_benchmark.json')
     #puts (build_response)
     if (build_response == nil)
       labels.push("no_data")
@@ -20,7 +20,8 @@ SCHEDULER.every '1h', :first_in => 0 do |job|
     else
       date = Date.parse(build_response['context']['date'][0,10])
       labels.push(date.strftime("%b %d"))
-      tpcc_data.push(build_response['benchmarks'][0]['items_per_second'].to_i)
+      # The second data set in this array is ScaleFactor4WithLogging
+      tpcc_data.push(build_response['benchmarks'][1]['items_per_second'].to_i)
     end
   end
   
